565e4b5d21080649aebb435349ec172c90df6ca7
milangupta95 <milangupta95@gmail.com>
2022-09-01 17:56:06 +0530

    Added Upto Login Form

--- reelsclone/src/Components/Login.js
+++ reelsclone/src/Components/Login.js
@@ -1,8 +1,25 @@
 import React from 'react'
-
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField'
+import { Box } from '@mui/system';
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <div className='corousel'>
+      </div>
+      <div className='loginform'>
+        <h1>Login</h1>
+        <form>
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <br/>
+          <TextField id="outlined-basic" label="Password" variant="outlined" />
+          <br/>
+          <Button variant="contained">
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
   )
 }
 
