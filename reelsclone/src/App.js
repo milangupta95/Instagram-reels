3f09e549168192ed1b76322da40dd4cc09f72470
milangupta95 <milangupta95@gmail.com>
2022-09-01 17:38:00 +0530

    SetUp Upto Router

--- reelsclone/src/App.js
+++ reelsclone/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {Routes,Route} from 'react-router-dom'
+import Login from './Components/Login.js';
+import SignUp from './Components/SignUp.js';
+import ForgetPassword from './Components/ForgetPassword.js';
+import ResetPassword from './Components/ResetPassword.js';
+import PageNotFound from './Components/PageNotFound.js';
+import Profile from './Components/Profile.js';
+import Navbar from './Components/Navbar.js';
+import Feed from './Components/Feed.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* Here Is Your App */}
+      <Navbar></Navbar>
+      <Routes>
+         <Route path = '/login' element = {<Login></Login>}></Route>
+         <Route path = '/singup' element = {<SignUp></SignUp>}></Route>
+         <Route path = '/forgetpassword' element = {<ForgetPassword></ForgetPassword>}></Route>
+         <Route path = '/resetpassword' element = {<ResetPassword></ResetPassword>}></Route>
+         <Route path = '/feed' element = {<Feed></Feed>}></Route>
+         <Route path = '/profile' element = {<Profile></Profile>}></Route>
+         <Route path = '/' element = {<PageNotFound></PageNotFound>}></Route>
+      </Routes> 
     </div>
   );
 }
